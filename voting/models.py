04ae97f1f26b8f8f0afff355e2929dbf48818062
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-11-14 17:28:32 +0100

    Created models from meeting

--- voting/models.py
+++ voting/models.py
@@ -1,11 +1,14 @@
 import re
 
+from django.contrib.auth.models import Group, User
 from django.db import models
 
 
 class Section(models.Model):
     name = models.TextField()
     program_codes = models.TextField()
+    user_group = models.ForeignKey(Group, null=True, related_name='+')
+    admin_group = models.ForeignKey(Group, null=True, related_name='+')
 
     def get_program_codes(self):
         items = re.split('[, \n]', self.program_codes)  # Split
@@ -13,3 +16,37 @@ class Section(models.Model):
         items = [x for x in items if x]                 # Filter
 
         return items
+
+
+class Meeting(models.Model):
+    name = models.CharField(max_length=64)
+    current_vote = models.ForeignKey('Vote', null=True, related_name='+')
+    section = models.ForeignKey(Section, null=False)
+    archived = models.BooleanField(default=False)
+
+
+class Scanner(models.Model):
+    user = models.ForeignKey(User, null=False)
+    meeting = models.ForeignKey(Meeting, null=False)
+
+
+class Attendant(models.Model):
+    user = models.ForeignKey(User, null=False)
+    meeting = models.ForeignKey(Meeting, null=False)
+
+
+class Vote(models.Model):
+    question = models.CharField(max_length=128)
+    open = models.BooleanField(default=True)
+    meeting = models.ForeignKey(Meeting, null=False)
+
+
+class Alternative(models.Model):
+    text = models.CharField(max_length=64)
+    num_votes = models.IntegerField(default=0)
+    vote = models.ForeignKey(Vote, null=False)
+
+
+class MadeVote(models.Model):
+    user = models.ForeignKey(User, null=False)
+    vote = models.ForeignKey(Vote, null=False)
