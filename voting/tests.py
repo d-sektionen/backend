3a55222e79586ba694b676f24265b9b93e9b523d
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-11-20 19:18:18 +0100

    Implemented correct voting functionality

--- voting/tests.py
+++ voting/tests.py
@@ -80,6 +80,47 @@ class VoteTest(AuthenticatedTestCase):
         return vote
 
 
+class VotingTest(AuthenticatedTestCase):
+    def setUp(self):
+        self.section = create_section('Section')
+        self.admin, self.client = create_admin([self.section])
+
+        self._create_vote(self.section)
+
+    def test_voting(self):
+        response = self.client.post('/voting/made_votes/', {'vote_id': self.vote.id, 'alternative_id': self.alternative1.id})
+
+        self.alternative1.refresh_from_db()
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(self.alternative1.num_votes, 1)
+
+    def test_double_voting(self):
+        response1 = self.client.post('/voting/made_votes/', {'vote_id': self.vote.id, 'alternative_id': self.alternative1.id})
+
+        self.alternative1.refresh_from_db()
+        self.assertEqual(response1.status_code, 204)
+        self.assertEqual(self.alternative1.num_votes, 1)
+
+        response2 = self.client.post('/voting/made_votes/', {'vote_id': self.vote.id, 'alternative_id': self.alternative1.id})
+
+        self.alternative1.refresh_from_db()
+        self.assertEqual(response2.status_code, 403)
+        self.assertEqual(self.alternative1.num_votes, 1)
+
+    def test_invalid_voting_request(self):
+        response = self.client.post('/voting/made_votes/', {'vote_id': self.vote.id + 1, 'alternative_id': self.alternative1.id})
+
+        self.alternative1.refresh_from_db()
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(self.alternative1.num_votes, 0)
+
+    def _create_vote(self, section):
+        meeting = Meeting.objects.create(name='Meeting 1', section=section)
+        self.vote = Vote.objects.create(question='Question?', meeting=meeting)
+        self.alternative1 = Alternative.objects.create(text='Alternative 1', vote=self.vote)
+        self.alternative2 = Alternative.objects.create(text='Alternative 2', vote=self.vote)
+
+
 class PerfectMeeeting(TestCase):
     @classmethod
     def setUpTestData(cls):
--- voting/urls.py
+++ voting/urls.py
@@ -8,7 +8,7 @@ router.register(r'meetings', views.MeetingViewSet, base_name='meeting')
 router.register(r'scanners', views.ScannerViewSet)
 router.register(r'attendants', views.AttendantViewSet)
 router.register(r'votes', views.VoteViewSet)
-router.register(r'made_votes', views.MadeVoteViewSet)
+router.register(r'made_votes', views.MadeVoteViewSet, base_name='made_vote')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
