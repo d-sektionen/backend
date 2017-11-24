c2824a95d87bb02b261d0bb5fdf82ea712a29924
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-11-24 14:50:41 +0100

    Allow redirection in token endpoint

--- account/views.py
+++ account/views.py
@@ -1,18 +1,27 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.shortcuts import redirect
 
 from rest_framework_jwt.settings import api_settings
 
 
 @login_required
 def generate_token(request):
-
     jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
     jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
     payload = jwt_payload_handler(request.user)
     token = jwt_encode_handler(payload)
 
-    return JsonResponse({
-        'token': token
-    })
+    if 'redirect' in request.GET:
+        redirect_url = request.GET['redirect']
+        if '?' in redirect_url:
+            redirect_url += '&token=' + token
+        else:
+            redirect_url += '?token=' + token
+
+        return redirect(redirect_url)
+    else:
+        return JsonResponse({
+            'token': token
+        })
