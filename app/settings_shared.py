dc1b6d1a62c71ae3c5e35fdd8ea3ca54e2f9f24f
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-11-24 14:35:17 +0100

    Serve static files in production

--- app/settings_shared.py
+++ app/settings_shared.py
@@ -105,7 +105,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # CAS
 CAS_SERVER_URL = "https://login.liu.se/cas/"
