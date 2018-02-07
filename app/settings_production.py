cff98bd22854963a96143e2b91070f38cbfeb155
Patrik Sletmo <patrik.sletmo@gmail.com>
2018-02-07 17:48:55 +0100

    Append to CHANNEL_LAYERS instead of modifying to keep other settings in sync

--- app/settings_production.py
+++ app/settings_production.py
@@ -41,12 +41,7 @@ LOGGING = {
 }
 
 # Currently configured for Heroku. Might want to re-configure for deployment on d-sektionen.se.
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        },
-        "ROUTING": "chat.routing.channel_routing",
-    },
+CHANNEL_LAYERS["default"]["BACKEND"] = "asgi_redis.RedisChannelLayer"
+CHANNEL_LAYERS["default"]["CONFIG"] = {
+    "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
 }
