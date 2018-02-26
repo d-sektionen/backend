c8cea3c8d5d48619f671ccc37c31322c1a6c51e1
angloth <angus.lothian@hotmail.com>
2018-02-26 19:59:01 +0100

    Add permission definitions to voting model classes

--- voting/models.py
+++ voting/models.py
@@ -10,6 +10,15 @@ class Meeting(models.Model):
     section = models.ForeignKey(Section, null=False)
     archived = models.BooleanField(default=False)
 
+    class Meta:
+        permissions = (
+            ("create_meeting", "Can create Meeting"),
+            ("read_meeting", "Can read Meeting"),
+            ("update_meeting", "Can update Meeting"),
+            ("delete_meeting", "Can delete Meeting"),
+            ("list_meetings", "Can list Meetings")
+        )
+
     def __str__(self):
         return self.name
 
@@ -20,6 +29,11 @@ class Scanner(models.Model):
 
     class Meta:
         unique_together = ('user', 'meeting')
+        permissions = (
+            ("create_scanner", "Can create Scanner"),
+            ("delete_scanner", "Can delete Scanner"),
+            ("list_scanners", "Can list Scanners")
+        )
 
 
 class Attendant(models.Model):
@@ -28,12 +42,26 @@ class Attendant(models.Model):
 
     class Meta:
         unique_together = ('user', 'meeting')
+        permissions = (
+            ("create_attendant", "Can create Attendant"),
+            ("delete_attendant", "Can delete Attendant"),
+            ("list_attendants", "Can list Attendants")
+        )
 
 
 class Vote(models.Model):
     question = models.CharField(max_length=128)
     open = models.BooleanField(default=True)
     meeting = models.ForeignKey(Meeting, null=False)
+
+    class Meta:
+        permissions = (
+            ("create_vote", "Can create Vote"),
+            ("read_vote", "Can read Vote"),
+            ("update_vote", "Can update Vote"),
+            ("delete_vote", "Can delete Vote"),
+            ("list_votes", "Can list Votes")
+        )
 
     def __str__(self):
         return self.question
@@ -44,7 +72,15 @@ class Alternative(models.Model):
     num_votes = models.IntegerField(default=0)
     vote = models.ForeignKey(Vote, null=False)
 
+    class Meta:
+        permissions = ()
+
 
 class MadeVote(models.Model):
     user = models.ForeignKey(User, null=False)
     vote = models.ForeignKey(Vote, null=False)
+
+    class Meta:
+        permissions = (
+            ("create_made_vote", "Can create MadeVote")
+        )
