a927e9fdf6e1823013323417b3a8fae752a4b48f
Samuel Johansson <samjo788@student.liu.se>
2018-03-12 18:09:49 +0100

    Added Storage to admin

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from storage.models import StorageRoom, Location, Booking, Object
+
+class StorageRoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'longitude','latitude')
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'room', 'can_contain_objects')
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('group', 'location', 'start_date','end_date', 'until_further_notice', 'description')
+
+class ObjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location','description', 'in_date', 'out_date', 'amount', 'belongs_to')
+
+admin.site.register(StorageRoom, StorageRoomAdmin)
+admin.site.register(Location,LocationAdmin)
+admin.site.register(Booking,BookingAdmin)
+admin.site.register(Object,ObjectAdmin)
