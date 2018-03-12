d7ace1459e817277c000a978b6700bcba17b511d
Patrik Sletmo <patrik.sletmo@gmail.com>
2018-03-12 19:32:10 +0100

    Cleaned up voting/views.py

--- voting/views.py
+++ voting/views.py
@@ -1,68 +1,26 @@
 from django.db import transaction
 from django.db.models import F
 from rest_framework import viewsets, status
-from rest_framework.decorators import list_route
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
-from app.decorators import extract_user, extract_username, extract_data
+
 from voting.models import Meeting, Attendant, Scanner, Vote, MadeVote, Alternative
 from voting.serializers import MeetingSerializer, AttendantSerializer, ScannerSerializer, VoteListSerializer, VoteDetailsSerializer, MeetingReadSerializer
+from voting.view_helpers import different_read_serializer, UserIdentifiableViewSet
 
 
+@different_read_serializer
 class MeetingViewSet(viewsets.ModelViewSet):
+    serializer_class = MeetingSerializer
+    read_serializer_class = MeetingReadSerializer
+
     def get_queryset(self):
         user = self.request.user
         user_groups = user.groups.all()
         meetings = Meeting.objects.filter(section__admin_group__in=user_groups)
 
         return meetings
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return MeetingReadSerializer
-
-        # The read serializer does not support foreign keys in requests properly,
-        # so return the normal serializer.
-        return MeetingSerializer
-
-
-class UserIdentifiableViewSet(viewsets.ModelViewSet):
-    check_section_membership = False
-
-    def get_model(self):
-        return self.serializer_class.Meta.model
-
-    @extract_user
-    def create(self, request, *args, **kwargs):
-        meeting_id = extract_data(request, 'meeting')
-        meeting = Meeting.objects.get(id=meeting_id)
-        user = kwargs['user']
-
-        # Verify that the user is a member of the section if configured to do so
-        if self.check_section_membership and not meeting.section.is_member(user):
-            return Response({'error': self.get_model().get_model_name() + ' måste tillhöra sektionen'}, status=status.HTTP_400_BAD_REQUEST)
-
-        attendant, created = self.get_model().objects.get_or_create(user=user, meeting=meeting)
-
-        if created:
-            return Response(self.serializer_class(attendant).data, status=status.HTTP_201_CREATED)
-        else:
-            return Response({'error': self.get_model().get_model_name() + ' redan registrerad'}, status=status.HTTP_400_BAD_REQUEST)
-
-    @list_route(methods=['delete'], url_path='')
-    @extract_username
-    def delete(self, request, *args, **kwargs):
-        meeting_id = extract_data(request, 'meeting')
-        meeting = Meeting.objects.get(id=meeting_id)
-        username = kwargs['username']
-
-        attendant = self.get_model().objects.filter(user__username=username, meeting=meeting).first()
-        if attendant is not None:
-            attendant.delete()
-            return Response({'status': 'ok'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': self.get_model().get_model_name() + ' inte registrerad på mötet'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AttendantViewSet(UserIdentifiableViewSet):
@@ -116,6 +74,7 @@ class MadeVoteViewSet(viewsets.ViewSet):
         if str(alternative.vote_id) != str(vote_id):
             return Response({'error': 'Omröstningen hittades inte'}, status=status.HTTP_404_NOT_FOUND)
 
+        # Update the reference by performing the addition directly in the database (using reference F)
         alternative.num_votes = F('num_votes') + 1
         alternative.save()
 
