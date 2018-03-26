8f3a5936bb4faca51a11b59cb132b27103e51f7a
Samuel Johansson <samjo788@student.liu.se>
2018-03-26 19:28:33 +0200

    Storage: Checks for booking conflicts

--- storage/models.py
+++ storage/models.py
@@ -16,15 +16,17 @@ class Booking(models.Model):
     group = models.ForeignKey(Group, null=False, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, null=False, on_delete=models.CASCADE)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
     until_further_notice = models.BooleanField()
     description = models.TextField()
 
     def clean(self):
-        if self.end_date < self.start_date:
-            raise ValidationError('start_date later than end_date')
         if self.end_date is not None and self.until_further_notice:
             raise ValidationError('end_date and until_further_notice must be mutually exclusive')
+        if self.end_date is None and not self.until_further_notice:
+            raise ValidationError('Must have end_date or until_further_notice')
+        if self.end_date is not None and self.end_date < self.start_date:
+            raise ValidationError('start_date later than end_date')
 
     def save(self, *args, **kwargs):
         self.full_clean()
