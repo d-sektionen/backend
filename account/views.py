0a5a5ade10d5fca1be2a0f00def7c45a648ab97b
Emil <emil19@users.noreply.github.com>
2019-07-29 11:04:43 +0200

    started work on user identification tokens

--- account/views.py
+++ account/views.py
@@ -2,11 +2,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.conf import settings
 from rest_framework import mixins, viewsets, status
 from rest_framework.decorators import list_route, action
 from rest_framework.response import Response
 
 from rest_framework_simplejwt.tokens import RefreshToken
+import jwt
+
+import datetime
 
 from .serializers import UserSerializer
 from .permissions import IsUser
@@ -38,4 +42,19 @@ class UserViewSet(
 
     def get_object(self):
         return self.request.user if self.kwargs['pk'] == 'me' else super().get_object()
+
+    @action(detail=False, methods=['get'])
+    def identification_token(self, request):
+        """
+        Returns a jwt token, for identifying a user, NOT to be used for auth.
+
+        Currently used to enable user identifying QR codes for the checkin app.
+        (The QR codes are generated and read client side)
+        """
+        # TODO: implement reading of identification codes.
+        expiry = datetime.datetime.utcnow() + datetime.timedelta(days=10)
+        expiry = expiry.replace(second=0, microsecond=0, minute=0, hour=6)
+        encoded = jwt.encode({'u': request.user.id, 'exp': expiry}, settings.SECRET_KEY, algorithm='HS256')
+
+        return Response({'token': encoded, 'expires': str(expiry)}, status.HTTP_200_OK)
 
