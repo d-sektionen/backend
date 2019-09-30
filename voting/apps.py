c91aededf0319f1637cdaaf9d16b2d57fa75c141
Emil <emil19@users.noreply.github.com>
2019-09-30 22:10:02 +0200

    disable sockets for voting

--- voting/apps.py
+++ voting/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class VotingConfig(AppConfig):
-    name = 'voting'
+    name = "voting"
 
-    def ready(self):
-        import voting.sockets
+    # def ready(self):
+    #     import voting.sockets
