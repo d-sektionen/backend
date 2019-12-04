eab02d1ea1be4c57787c3c54f2c1b40902d9e45d
aleno523 <aleno523@student.liu.se>
2019-12-04 19:26:47 +0100

    tfw you remove some code and break all test cases

--- account/tests.py
+++ account/tests.py
@@ -7,18 +7,12 @@ from rest_framework.test import APIClient
 
 num_users = 0
 
-
-# def create_section(name):
-#     return Section.objects.create(name=name)
-
-
 def create_user():
     user = User.objects.create_user(username=_next_username(), password='Password123')
 
     client = APIClient()
     client.login(username=user.username, password='Password123')
 
-    # vet inte om mina ändringar är vettiga
     token_response = client.get('/account/token/')
     token_data = json.loads(token_response.content.decode('utf-8'))
 
@@ -29,9 +23,6 @@ def create_user():
 
 
 def create_admin():
-    # TODO: should probably be a specific admin type.
-    #user, client = create_user()
-    # TODO: Make the dude an admin.
     #return user, client
 
     user = User.objects.create_superuser(username=_next_username(), email=None, password='Password123')
@@ -52,3 +43,7 @@ def _next_username():
     global num_users
     num_users += 1
     return 'user' + str(num_users)
+
+class AuthenticatedTestCase(TestCase):
+    def setUp(self):
+        self.user, self.client = create_user()
