16e3303bd312f52b7dbc445ce499faadd1e919a1
Emil <emil19@users.noreply.github.com>
2020-01-15 15:32:58 +0100

    add uwsgi_tasks

---
+++ app/uwsgi_tasks.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.core import management
+
+from uwsgidecorators import *
+from datetime import datetime
+
+@cron(10, 3, -1, -1, -1)
+def nightly(num):
+    print(f"Running nightly command. {datetime.now()}")
+    management.call_command('nightly')
