f9a443c132f10d534c34af6016326a9bf615c831
Emil <emil19@users.noreply.github.com>
2020-02-11 20:54:01 +0100

    voting view updates

--- voting/views.py
+++ voting/views.py
@@ -1,36 +1,67 @@
 from django.db import transaction
 from django.db.models import F, Q
-from rest_framework import mixins, viewsets, status
-from rest_framework.exceptions import ValidationError
+from rest_framework import mixins, viewsets, status, serializers, exceptions
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from app.permissions import FixedDjangoModelPermissions
 from .permissions import VotePermission
 
 from .models import Meeting, Attendant, Vote, MadeVote, Alternative
-from .serializers import MeetingSerializer, AttendantSerializer, VoteListSerializer, VoteDetailsSerializer
+from .serializers import (
+    MeetingSerializer,
+    AttendantSerializer,
+    VoteListSerializer,
+    VoteDetailsSerializer,
+)
 
-class NoDeleteViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+
+class NoDeleteViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
     pass
+
 
 class MeetingViewSet(NoDeleteViewSet):
     queryset = Meeting.objects.filter(archived=False)
     serializer_class = MeetingSerializer
-    permission_classes = (FixedDjangoModelPermissions,) 
+    permission_classes = (FixedDjangoModelPermissions,)
 
-class AttendantViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+
+class AttendantViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Attendant.objects.all()
     serializer_class = AttendantSerializer
     permission_classes = (FixedDjangoModelPermissions,)
 
-    def get_queryset(self):
-        if self.request.method == 'GET':
-            if 'meeting_id' not in self.request.query_params:
-                raise ValidationError(detail='Missing required parameter "meeting_id"')
-            meeting_id = self.request.query_params['meeting_id']
+    def get_queryset(self, meeting_specific=False):
+        if self.request.method == "GET" or meeting_specific:
+            if "meeting_id" not in self.request.query_params:
+                raise exceptions.ParseError(
+                    detail='Missing required parameter "meeting_id"'
+                )
+                # raise exceptions.ValidationError(
+                #     detail='Missing required parameter "meeting_id"'
+                # )
+            meeting_id = self.request.query_params["meeting_id"]
             return Attendant.objects.filter(meeting_id=meeting_id)
 
         return Attendant.objects.all()
+
+    @action(detail=False, methods=["delete"])
+    def clear(self, request, pk=None):
+        attendants = self.get_queryset(meeting_specific=True)
+        attendants.delete()
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class VoteViewSet(NoDeleteViewSet):
     serializer_class = VoteListSerializer
@@ -50,11 +81,14 @@ class VoteViewSet(NoDeleteViewSet):
         """
 
         user = self.request.user
-        if 'current' in request.query_params and request.query_params['current'] == 'true':
+        if (
+            "current" in request.query_params
+            and request.query_params["current"] == "true"
+        ):
             meetings = Meeting.objects.filter(attendant__user__in=[user])
             vote_ids = [x.id for x in filter(None, [x.current_vote for x in meetings])]
-            votes = Vote.objects.filter(id__in=vote_ids).order_by('-id')
-        elif user.has_perm('voting.view_vote'):
+            votes = Vote.objects.filter(id__in=vote_ids).order_by("-id")
+        elif user.has_perm("voting.view_vote"):
             # Show everything for an admin
             votes = Vote.objects.all()
         else:
@@ -67,25 +101,35 @@ class VoteViewSet(NoDeleteViewSet):
 class MadeVoteViewSet(viewsets.ViewSet):
     @transaction.atomic  # Added to ensure that we don't end up with a plus-oned alternative but no existing record of it.
     def create(self, request):
-        vote_id = request.data['vote_id']
-        alternative_id = request.data['alternative_id']
+        vote_id = request.data["vote_id"]
+        alternative_id = request.data["alternative_id"]
 
         alternative = Alternative.objects.get(id=alternative_id)
         if str(alternative.vote_id) != str(vote_id):
-            return Response({'error': 'Omröstningen hittades inte'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "Omröstningen hittades inte"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
         vote = Vote.objects.get(id=vote_id)
-        if not Attendant.objects.filter(meeting=vote.meeting, user=request.user).exists():
-            return Response({'error': 'Du måste närvara på mötet för att få rösta'}, status=status.HTTP_403_FORBIDDEN)
+        if not Attendant.objects.filter(
+            meeting=vote.meeting, user=request.user
+        ).exists():
+            return Response(
+                {"error": "Du måste närvara på mötet för att få rösta"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
         if MadeVote.objects.filter(vote_id=vote_id, user=request.user).exists():
-            return Response({'error': 'Du har redan röstat i den här omröstningen'}, status=status.HTTP_403_FORBIDDEN)
-
+            return Response(
+                {"error": "Du har redan röstat i den här omröstningen"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
         # Update the reference by performing the addition directly in the database (using reference F)
-        alternative.num_votes = F('num_votes') + 1
+        alternative.num_votes = F("num_votes") + 1
         alternative.save()
 
         MadeVote.objects.create(vote_id=vote_id, user=request.user)
 
-        return Response({'status': 'ok'}, status=status.HTTP_200_OK)
+        return Response({"status": "ok"}, status=status.HTTP_200_OK)
