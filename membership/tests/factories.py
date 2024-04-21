7e1c2d82dac51db83a14239e7434fd9669d1396b
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2024-04-21 16:59:22 +0200

    Add factories for membership

---
+++ membership/tests/factories.py
@@ -0,0 +1,11 @@
+from factory import django, Faker, fuzzy
+from membership.models import Member
+
+class MemberFactory(django.DjangoModelFactory):
+    class Meta:
+        model = Member
+
+    liu_id = Faker("lexify", letters="abcdefghijklmnopqrstuvwxyz", text=Faker("numerify", text="?????###"))  # Get a believable LiU ID
+    first_name = Faker("first_name")
+    last_name = Faker("last_name")
+    membership_type = fuzzy.FuzzyChoice(Member.MEMBERSHIP_TYPE_CHOICES, getter=lambda c: c[0])
