27bf9956850651f801500c5348985c80c09a0448
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2024-07-13 11:23:15 +0200

    Add status endpoint
    Used by Upptime. Currently only returns static "UP" response but could be extended with more
      info later.

--- tools/urls.py
+++ tools/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
-
+from django.conf.urls import url, include
+from rest_framework import routers
 from . import views
 
 
+router = routers.DefaultRouter()
+router.register(r"status", views.StatusViewSet, basename="status")
 urlpatterns = [
+    url(r"^", include(router.urls)),
     url(r"calendar", views.section_calendar),
 ]
