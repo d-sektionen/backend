8c73aeb1d5d20c4afdd84e986aa9124ffcd90484
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2024-07-14 23:44:22 +0200

    Replace psycopg2 specific engine

--- src/app/settings_production.py
+++ src/app/settings_production.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_NAME", "django"),
         "USER": os.getenv("POSTGRES_USER", "django"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "django"),
