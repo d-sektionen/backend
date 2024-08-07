c9638a18ddeeec64ee57b6812aae56c842adb633
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2024-08-07 16:16:57 +0200

    Fix linter error introduced when removing storage

--- src/app/permissions.py
+++ src/app/permissions.py
@@ -1,17 +1,18 @@
-from rest_framework.permissions import IsAuthenticated, BasePermission, DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 
 class AllowOptionsAuthentication(IsAuthenticated):
     def has_permission(self, request, view):
-        if request.method == 'OPTIONS':
+        if request.method == "OPTIONS":
             return True
         return request.user and request.user.is_authenticated
+
 
 class FixedDjangoModelPermissions(DjangoModelPermissions):
     """
     DjangoModelPermissions does not handle view permissions from django 2.1,
     this fixes that.
     """
-    def __init__(self):
-        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
 
+    def __init__(self):
+        self.perms_map["GET"] = ["%(app_label)s.view_%(model_name)s"]
