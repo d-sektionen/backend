9ff1392987616387fe5034696e87e6c785992fb9
wind1s <44732263+wind1s@users.noreply.github.com>
2024-08-07 22:38:51 +0200

    Add protection prompt push_image and deploy scripts to avoid accidental production push/deploy. Also refactor code along with change.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,41 @@
 #!/usr/bin/env bash
 
+set -e
+
 arg=$1
 # Change this to the account you have access with.
 server_account="webbu"
-server_url="ssh.new.d-sektionen.se"
+server_fqdn="ssh.new.d-sektionen.se"
 # Add the webhooks from respective stack in container.
-backend_dev_webhook="REPLACE_ME_WITH_WEBHOOK"
-backend_prod_webhook="REPLACE_ME_WITH_WEBHOOK"
+backend_dev_webhook_url="REPLACE_ME_WITH_WEBHOOK"
+backend_prod_webhook_url="REPLACE_ME_WITH_WEBHOOK"
+
+RED="\e[91m"
+ENDCOLOR="\e[0m"
+
+prompt_yes_no() {
+    while true; do
+        echo -en "$1 [y/n]: "
+        read yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+        esac
+    done
+}
 
 if [ "$arg" = "dev" ]; then
-    webhook=$backend_dev_webhook
+    webhook_url=$backend_dev_webhook_url
+
 elif [ "$arg" = "prod" ]; then
-    webhook=$backend_prod_webhook
+    prompt_yes_no "Do you really want to deploy to ${RED}PRODUCTION${ENDCOLOR}?"
+    webhook_url=$backend_prod_webhook_url
+
 else
-    echo "First argument should be either 'dev' or 'prod'"
+    echo -e "First argument should be either ${RED}dev${ENDCOLOR} or ${RED}prod${ENDCOLOR}"
     exit
 fi
 
 set -x
 # Assumes you have ssh keys to the server account.
-ssh $server_account@$server_url "curl -X POST $webhook"
+ssh $server_account@$server_fqdn "curl -X POST $webhook_url"
--- push_image.sh
+++ push_image.sh
@@ -1,21 +1,45 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+set -e
+
 arg=$1
-registry_url="registry.d-sektionen.se"
+# Change these if necessary.
+registry_fqdn="registry.d-sektionen.se"
+image_name="backend"
 backend_dev_tag="development"
 backend_prod_tag="latest"
 
+RED="\e[91m"
+ENDCOLOR="\e[0m"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+prompt_yes_no() {
+    while true; do
+        echo -en "$1 [y/n]: "
+        read yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+        esac
+    done
+}
+
 if [ "$arg" = "dev" ]; then
-    tag=$backend_dev_tag
+    image_tag=$backend_dev_tag
+
 elif [ "$arg" = "prod" ]; then
-    tag=$backend_prod_tag
+    prompt_yes_no "Do you really want to push to ${RED}PRODUCTION${ENDCOLOR}?"
+    image_tag=$backend_prod_tag
+
 else
-    echo "First argument should be either 'dev' or 'prod'"
+    echo -e "First argument should be either ${RED}dev${ENDCOLOR} or ${RED}prod${ENDCOLOR}"
     exit
 fi
 
+docker_image="$registry_fqdn/$image_name:$image_tag"
+
 set -x
-docker login https://$registry_url
-docker build -t $registry_url/backend:$tag -f $SCRIPT_DIR/Dockerfile .
-docker push $registry_url/backend:$tag
+# Get login credentials from webmaster
+docker login https://$registry_fqdn
+docker build -t $docker_image -f $SCRIPT_DIR/Dockerfile .
+docker push $docker_image
