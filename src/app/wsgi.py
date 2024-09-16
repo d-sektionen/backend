fb71570cf6812d2cddd5e0fcafb4abce191fae88
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2024-09-16 21:47:07 +0200

    Add uwsgi timer to send queued emails periodically

--- src/app/wsgi.py
+++ src/app/wsgi.py
@@ -14,3 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings_production")
 
 application = get_wsgi_application()
+
+try:
+    import uwsgidecorators
+    from django.core.management import call_command
+
+    @uwsgidecorators.timer(100)
+    def send_queued_mail(num):
+        """Send queued mail every 10 seconds"""
+        call_command("send_queued_mail", processes=1)
+
+except ImportError:
+    print("uwsgidecorators not found. Cron and timers are disabled")
