8eb558015416e11b91148bc66f7a742c34f701f1
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2024-09-30 11:52:11 +0200

    Add default email setting

--- src/app/settings_shared.py
+++ src/app/settings_shared.py
@@ -223,3 +223,4 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT", 587)
 EMAIL_HOST_USER = os.getenv("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@d-sektionen.se")
