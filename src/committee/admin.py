c0378d34941cf85850dab59bdff91743cd1baf81
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2024-12-03 18:08:12 +0100

    Update committee admin display to reflect model changes

--- src/committee/admin.py
+++ src/committee/admin.py
@@ -1,24 +1,18 @@
 from django.contrib import admin
 
-from committee.forms import CommitteeForm
-from committee.models import Committee
+from committee.models import Committee, CommitteeMember
 
 
 class CommitteeAdmin(admin.ModelAdmin):
-    add_form = CommitteeForm
-    change_form = CommitteeForm
-
-    def get_form(self, request, obj=None, **kwargs):
-        self.form = self.change_form if obj else self.add_form
-        return super().get_form(request, obj, **kwargs)
-
-    def get_fieldsets(self, request, obj=None, **kwargs):
-        self.fieldsets = self.change_form.Meta.fieldsets if obj else self.add_form.Meta.fieldsets
-        return super().get_fieldsets(request, obj, **kwargs)
-
-    list_display = ("id", "name", "description", "treasurer", "treasurer_email",  "chair", "chair_email")
-    list_filter = ("treasurer", "chair")
-    search_fields = ("name", "description", "treasurer", "chair")
+    list_display = ("name",)
+    search_fields = ("name",)
     ordering = ("name",)
+    filter_horizontal = (
+        "chairman_permissions",
+        "treasurer_permissions",
+        "other_permissions",
+    )
 
+
+admin.site.register(CommitteeMember)
 admin.site.register(Committee, CommitteeAdmin)
