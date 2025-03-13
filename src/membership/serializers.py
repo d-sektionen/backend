3ae0c749689056b250a8e0f59dedeed2e77cb94b
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2025-03-13 01:16:38 +0100

    Add ability to pass liu_card_id when submitting membership request

--- src/membership/serializers.py
+++ src/membership/serializers.py
@@ -12,6 +12,8 @@ class RequestSerializer(serializers.ModelSerializer):
         default=False, required=False, write_only=True
     )
 
+    liu_card_id = serializers.CharField(required=False, max_length=17, write_only=True)
+
     class Meta:
         model = Request
         fields = (
@@ -22,10 +24,17 @@ class RequestSerializer(serializers.ModelSerializer):
             "starting_year",
             "message",
             "infomail_subscriber",
+            "liu_card_id",
         )
 
     def create(self, validated_data):
         user = self.context["request"].user
         user.profile.infomail_subscriber = validated_data.pop("infomail_subscriber")
+
+        if validated_data.get("liu_card_id"):
+            user.profile.liu_card_id = validated_data.pop("liu_card_id")
+
         user.profile.save()
+        print(validated_data)
+
         return super().create(validated_data)
