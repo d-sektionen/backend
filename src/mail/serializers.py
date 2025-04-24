1a1ec7f9c60cd6c08157afd8b134d753722bc856
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2025-04-24 20:09:28 +0200

    Include rendered template in MailTemplateSerializer

--- src/mail/serializers.py
+++ src/mail/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import MailTemplate, Mail
+from django.template.loader import render_to_string
+from datetime import date
+from .view_helpers import getEventData
 import nh3
 
 
@@ -9,15 +12,33 @@ class InfomailSerializer(serializers.Serializer):
 
 
 class MailTemplateSerializer(serializers.ModelSerializer):
+    template = serializers.SerializerMethodField()
+
+    def get_context_data(self, **kwargs):
+        context = {}
+
+        context["week_number"] = date.today().isocalendar()[1]
+        context["events"] = getEventData()
+
+        return context
+
+    def get_template(self, obj):
+        context = self.get_context_data()
+        html_code = render_to_string(obj.template_filename, context).strip()
+
+        return html_code
+
     class Meta:
         model = MailTemplate
-        fields = ["id", "name", "category", "subject", "description"]
+        fields = ["id", "name", "category", "subject", "description", "template"]
 
 
 class MailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mail
-        fields = ["id", "subject", "html", "sendAt", "category", "sendTo"]
+        fields = ["id", "subject", "html", "category"]
+
+
 class SendMailSerializer(serializers.ModelSerializer):
     send_at = serializers.DateTimeField(required=False)
 
