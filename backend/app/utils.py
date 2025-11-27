423320b72e88d10fac13679ce6879a52b3b2bbdb
Otto Roming <otto.roming@protonmail.com>
2025-11-27 18:06:45 +0100

    improved render_email documentation

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,8 +1,18 @@
 from django.template.loader import get_template
+from typing import Any, Tuple
 
 
-def render_email(template_name, context={}):
-    """Render an email from a template."""
+def render_email(template_name: str, context: Any = {}) -> Tuple[str, str]:
+    """Render an email from two templates template.
+    This function assumes that for template name both <template_name>.html and <template_name>.subject.txt
+    exsists to generate both the body of the email and the subject.
+
+    Args:
+        template_name (str): the base of the template names
+        contenxt (Any): the context provided to templates
+    Returns:
+        Tuple[str, str]: subject and html content of the email
+    """
     subject_template = get_template(f"{template_name}.subject.txt")
     html_template = get_template(f"{template_name}.html")
 
