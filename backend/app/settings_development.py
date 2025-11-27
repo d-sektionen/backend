72713e79888eef6c9a5050478c94f66ccb547a06
Markus Piotrowski <markuspiotrowski@live.com>
2025-11-27 20:06:35 +0100

    Use ENV variables for container data

--- backend/app/settings_development.py
+++ backend/app/settings_development.py
@@ -14,11 +14,12 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': None # Use in-memory DB
-            }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
