8aeca2d0572f53325da070a887f3d22f54fd2899
Otto Roming <otto.roming@protonmail.com>
2025-12-16 19:35:27 +0100

    fixe variable naming

--- backend/app/settings_shared.py
+++ backend/app/settings_shared.py
@@ -232,31 +232,31 @@ EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@d-sektionen.se")
 
-INFO_D_SEKTIONEN_WEBSITE_URL = os.getenv(
+INFO_DSEKTIONEN_WEBSITE_URL = os.getenv(
     "DSEKTIONEN_WEBSITE_URL", "https://d-sektionen.se"
 )
-INFO_D_SEKTIONEN_INFO_EMAIL = os.getenv("DSEKTIONEN_INFO_EMAIL", "info@d-sektionen.se")
-INFO_D_SEKTIONEN_GDPR_URL = os.getenv(
+INFO_DSEKTIONEN_INFO_EMAIL = os.getenv("DSEKTIONEN_INFO_EMAIL", "info@d-sektionen.se")
+INFO_DSEKTIONEN_GDPR_URL = os.getenv(
     "DSEKTIONEN_GDPR_URL",
     "https://d-sektionen.se/wp-content/uploads/2018/05/Policy-datahantering-D-sektionen.pdf",
 )
-INFO_D_SEKTIONEN_LOGO_URL = os.getenv(
+INFO_DSEKTIONEN_LOGO_URL = os.getenv(
     "DSEKTIONEN_LOGO_URL",
     "https://d-sektionen.se/file_archive/sektionsdokument/logotyper/Color_white.png",
 )
-INFO_D_SEKTIONEN_UNSUBSCRIBE_URL = os.getenv(
+INFO_DSEKTIONEN_UNSUBSCRIBE_URL = os.getenv(
     "UNSUBSCRIBE_URL", "https://medlem.d-sektionen.se/preferences/"
 )
-INFO_D_SEKTIONEN_INSTAGRAM_URL = os.getenv(
+INFO_DSEKTIONEN_INSTAGRAM_URL = os.getenv(
     "INSTAGRAM_URL", "https://www.instagram.com/dsektionen/"
 )
-INFO_D_SEKTIONEN_FACEBOOK_URL = os.getenv(
+INFO_DSEKTIONEN_FACEBOOK_URL = os.getenv(
     "FACEBOOK_URL", "https://www.facebook.com/datateknologsektionen"
 )
-INFO_D_SEKTIONEN_FACEBOOK_GROUP_URL = os.getenv(
+INFO_DSEKTIONEN_FACEBOOK_GROUP_URL = os.getenv(
     "FACEBOOK_GROUP_URL", "https://www.facebook.com/groups/112745532123135"
 )
-INFO_D_SEKTIONEN_MORE_SOCIAL_MEDIA_URL = os.getenv(
+INFO_DSEKTIONEN_MORE_SOCIAL_MEDIA_URL = os.getenv(
     "MORE_SOCIAL_MEDIA_URL", "https://d-sektionen.se/socialamedier/"
 )
 INFO_CALENDAR_ICAL_URL = os.getenv(
