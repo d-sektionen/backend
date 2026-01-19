8ec4de59b6009559bffc1e916058111d7ba43d16
Markus Piotrowski <markuspiotrowski@live.com>
2026-01-19 20:01:28 +0100

    Mabye fix

--- backend/app/settings_test.py
+++ backend/app/settings_test.py
@@ -15,11 +15,7 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_NAME"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
     }
 }
