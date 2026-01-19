628d3d6fe3f41c1ccf69b5c55434ec9061c8b28a
Markus Piotrowski <markuspiotrowski@live.com>
2026-01-19 19:50:10 +0100

    Add possible fix for tests

--- backend/app/settings_development.py
+++ backend/app/settings_development.py
@@ -1,14 +1,14 @@
 from dotenv import load_dotenv
 from .settings_shared import *
 
-dotenv_path = os.path.join(BASE_DIR, '.env')
+dotenv_path = os.path.join(BASE_DIR, ".env")
 
 load_dotenv(dotenv_path)
 
-SECRET_KEY = '*3#2cxri$uc!5%#v+-9!h=yig-$@i-e!idod(d&9v6qf)bjv%!'
+SECRET_KEY = "*3#2cxri$uc!5%#v+-9!h=yig-$@i-e!idod(d&9v6qf)bjv%!"
 DEBUG = True
 EMAIL_ENABLED = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
@@ -21,5 +21,8 @@ DATABASES = {
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT"),
+        "TEST": {
+            "NAME": None,
+        },
     }
 }
