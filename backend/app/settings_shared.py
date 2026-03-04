51de6ee0315828a0659f4fd359c072025ca2a316
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2026-03-04 21:38:19 +0100

    Rename APP_HOSTNAME->BASE_URL and include protocol

--- backend/app/settings_shared.py
+++ backend/app/settings_shared.py
@@ -5,6 +5,7 @@ Overwriting settings sometimes happens in sub setting files.
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from urllib.parse import urlparse
 import datetime
 from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
@@ -65,7 +66,8 @@ MIDDLEWARE = [
 ]
 
 MICROSOFT_LOGIN_HOST = "login.microsoftonline.com"
-APP_HOSTNAME = os.getenv("APP_HOSTNAME")
+BASE_URL = os.getenv("BASE_URL")
+APP_HOSTNAME = urlparse(BASE_URL).hostname
 
 ALLOWED_HOSTS = [
     APP_HOSTNAME,
