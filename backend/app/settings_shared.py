5acbbf26fd88794075eb9e33ff06f9b350728736
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2026-03-04 21:39:51 +0100

    Give templates the ability to access settings (only BASE_URL at the moment)

--- backend/app/settings_shared.py
+++ backend/app/settings_shared.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "backend.app.context_processors.export_settings",
             ]
         },
     },
