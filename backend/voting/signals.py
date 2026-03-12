5b4645784df179e55c56e523b4e6d0e3accbe5f3
Markus Piotrowski <markuspiotrowski@live.com>
2026-03-12 19:57:08 +0100

    socket for attendantPannel

--- backend/voting/signals.py
+++ backend/voting/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import SpeakerRequest
+from .models import SpeakerRequest, Attendant
 import asyncio
 from backend.app.sockets import sio
 
@@ -41,5 +41,44 @@ def speaker_request_deleted(sender, instance, **kwargs):
                 "meeting_id": instance.meeting.id,
             },
             room=f"meeting_speker_{instance.meeting.id}",
+        )
+    )
+
+
+@receiver(post_save, sender=Attendant)
+def new_attendant(sender, instance, created, **kwargs):
+    if created:
+        data = {
+            "id": instance.id,
+            "user": {
+                "id": instance.user.id,
+                "username": instance.user.username,
+                "first_name": instance.user.first_name,
+                "last_name": instance.user.last_name,
+                "pretty_name": instance.user.get_full_name(),
+            },
+            "has_voting_rights": instance.has_voting_rights,
+            "meeting_id": instance.meeting.id,
+        }
+
+        asyncio.run(
+            sio.emit(
+                "new_attendant",
+                data,
+                room=f"meeting_attendants_{instance.meeting.id}",
+            )
+        )
+
+
+@receiver(post_delete, sender=Attendant)
+def attendant_deleted(sender, instance, **kwargs):
+    asyncio.run(
+        sio.emit(
+            "delete_attendant",
+            {
+                "attendant_id": instance.id,
+                "meeting_id": instance.meeting.id,
+            },
+            room=f"meeting_attendants_{instance.meeting.id}",
         )
     )
