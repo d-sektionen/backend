cea0646d5d52ad68d47d8f3d7d892cd2a10dfa43
Markus Piotrowski <markuspiotrowski@live.com>
2026-04-17 14:08:28 +0200

    Update logging files

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,19 @@
 # gunicorn.conf.py
 bind = "0.0.0.0:8000"
+
 workers = 4
 worker_class = "uvicorn.workers.UvicornWorker"
+
 max_requests = 5000
 max_requests_jitter = 500
 timeout = 20
 graceful_timeout = 60
-accesslog = "/var/log/backend.log"
+keepalive = 5
+
+accesslog = "/var/log/backend_access.log"
+errorlog = "/var/log/backend_error.log"
+loglevel = "info"
+
 limit_request_line = 65535
 limit_request_field_size = 65535
 
